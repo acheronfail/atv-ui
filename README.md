fae1b781fa1afc2044c6607edaabb26f3ba61732
acheronfail <acheronfail@gmail.com>
2020-05-16 20:48:13 +1000

    chore: update getting started guide

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # `atv-ui`
 
 ```bash
+# Install dependencies
 yarn install
+
+# Pair with AppleTV
+yarn appletv pair
+
+# Write credentials into .credentials.json
+echo '"00000000-0000-0000-0000-000000000000:0000..."' > .credentials.json
+
+# Start the server
 yarn start
+
 # Now navigate to your server's address, eg: http://192.168.1.100:8080 on your touch device
+# :)
 ```
 
--- src/atv/credentials.ts
+++ src/atv/credentials.ts
@@ -4,8 +4,5 @@ import { Credentials, parseCredentials } from 'node-appletv';
 
 export async function getCredentials(): Promise<Credentials> {
   const credentialsFile = path.join(process.cwd(), '.credentials.json');
-  const credentialsList: string[] = await fs.readJson(credentialsFile);
-  const credentials = credentialsList.map(parseCredentials);
-
-  return credentials[0];
+  return parseCredentials(await fs.readJson(credentialsFile));
 }
